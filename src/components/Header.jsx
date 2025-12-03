884efc3fac84f78b6957b07c4053f81826c57e79
Spartak092225 <spartak092225@gmail.com>
2025-12-03 13:38:32 +0100

    Create a dropdown fro the units and add styling

--- src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-export function Header() {
-  return (
-    <div className="header">
-      <h1>hello</h1>
-    </div>
-  );
-}
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,57 @@
+import styles from "./Header.module.css";
+
+export function Header() {
+  return (
+    <div className={styles.header}>
+      <img src="logo.svg" alt="logo" />
+      <div className={styles.unitsSelectorContainer}>
+        <img src="icon-units.svg" alt="gear icon" />
+        <p>Units</p>
+        <img src="icon-dropdown.svg" alt="" />
+
+        <div className={styles.dropdownContent}>
+          <h4>Switch to Imperial</h4>
+          <fieldset>
+            <legend>Temperature</legend>
+            <div className={styles.temperatureOptions}>
+              <label htmlFor="celsius">
+                Celsius (&#8451;)
+                <input type="radio" id="celsius" name="temperature" />
+              </label>
+
+              <label htmlFor="fahrenheit">
+                Fahrenheit (&#8457;){" "}
+                <input type="radio" id="fahrenheit" name="temperature" />
+              </label>
+            </div>
+          </fieldset>
+
+          <fieldset>
+            <legend>Wind Speed</legend>
+            <div className={styles.windSpeedOptions}>
+              <label htmlFor="kmh">
+                km/h <input type="radio" id="kmh" name="speed" />
+              </label>
+              <label htmlFor="mph">
+                mph <input type="radio" id="mph" name="speed" />
+              </label>
+            </div>
+          </fieldset>
+          <fieldset>
+            <legend>Precipitation</legend>
+            <div className={styles.precipitationOptions}>
+              <label htmlFor="mm">
+                Millimeters (mm){" "}
+                <input type="radio" id="mm" name="precipitation" />
+              </label>
+              <label htmlFor="inches">
+                Inches (in){" "}
+                <input type="radio" id="inches" name="precipitation" />
+              </label>
+            </div>
+          </fieldset>
+        </div>
+      </div>
+    </div>
+  );
+}
