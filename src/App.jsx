1b007c4a97706add66e63083287052791ca8d534
Spartak092225 <spartak092225@gmail.com>
2025-12-03 10:49:48 +0100

    Create the project and import image assest and design guides.

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import "./App.css";
+import { Header } from "./components/Header";
+
+export default function App() {
+  return <Header />;
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <div className="header">
+      <h1>hello</h1>
+    </div>
+  );
+}
