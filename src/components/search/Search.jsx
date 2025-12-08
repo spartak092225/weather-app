b244dccdcbbbc0028a3bd952bfe1e5235f5a7d5a
Spartak092225 <spartak092225@gmail.com>
2025-12-08 09:47:58 +0100

    Add open/close  logic to the search dropdown.

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,14 +1,41 @@
+import { useState, useRef, useEffect } from "react";
 import styles from "./Search.module.css";
 
 export function Search() {
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+  const containerRef = useRef(null);
+
+  function handleClickOutside(event) {
+    if (containerRef.current && !containerRef.current.contains(event.target)) {
+      setIsDropdownVisible(false);
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className={styles.searchSection}>
       <h1>How's the sky looking today?</h1>
       <div className={styles.searchBarContainer}>
-        <div>
-          <input type="search" placeholder="Search for a place" />
+        <div ref={containerRef}>
+          <input
+            type="search"
+            placeholder="Search for a place"
+            onFocus={() => setIsDropdownVisible(true)}
+            onChange={() => setIsDropdownVisible(true)}
+          />
           <img src="icon-search.svg" alt="" />
-          <div className={styles.searchDropdown}>
+
+          <div
+            className={`${styles.searchDropdown} ${
+              isDropdownVisible ? styles.searchDropdownOpen : ""
+            }`}
+          >
             <p>City name</p>
             <p>City name</p>
             <p>City name</p>
